fb3bb669dc7bb0f98b32e8b5037fcef2979fb807
rj7075 <rj7075yadav.com>
2025-09-10 20:11:18 +0530

    file updated

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -139,7 +139,7 @@ const Pricing = () => {
                 <div className="absolute -top-4 left-1/2 transform -translate-x-1/2">
                   <div className="bg-gradient-to-r from-blue-500 to-purple-600 rounded-full px-6 py-2 flex items-center space-x-2">
                     <Crown />
-                    <span className="text-sm font-semibold text-gray-200">
+                    <span className="text-xs font-semibold text-gray-200">
                       Most Popular
                     </span>
                   </div>
@@ -169,7 +169,7 @@ const Pricing = () => {
               </ul>
               <a href={plan.link} target="_blank">
                 <button
-                  className={`w-full py-4 text-lg font-semibold rounded-lg group ${
+                  className={`w-full py-1 text-lg font-semibold rounded-lg group ${
                     plan.popular
                       ? "bg-gradient-to-r mt-2 from-blue-500 to-purple-600 text-white shadow-md hover:shadow-lg transition-shadow"
                       : "bg-gray-100 text-gray-900 hover:bg-gradient-to-r hover:from-blue-400 hover:to-blue-600 hover:text-white transition-all"
@@ -191,22 +191,21 @@ const Pricing = () => {
             <h3 className="text-2xl font-bold mb-4 text-purple-600">
               All Plans Include
             </h3>
-            
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-6 text-gray-400">
-  <div className="flex justify-start items-center text-left space-x-2">
-    <CheckCircle />
-    <span>No Setup Fees</span>
-  </div>
-  <div className="flex justify-start items-center text-left space-x-2">
-    <CheckCircle />
-    <span>Cancel Anytime</span>
-  </div>
-  <div className="flex justify-start items-center text-left space-x-2">
-    <CheckCircle />
-    <span>30-Day Money Back</span>
-  </div>
-</div>
 
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6 text-gray-400">
+              <div className="flex justify-start items-center text-left space-x-2">
+                <CheckCircle />
+                <span>No Setup Fees</span>
+              </div>
+              <div className="flex justify-start items-center text-left space-x-2">
+                <CheckCircle />
+                <span>Cancel Anytime</span>
+              </div>
+              <div className="flex justify-start items-center text-left space-x-2">
+                <CheckCircle />
+                <span>30-Day Money Back</span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
