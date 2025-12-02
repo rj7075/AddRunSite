695ed4250278a79b43073e9ccfa78040c0fb81d7
Ranjeet Yadav <rj7075yadav@gmail.com>
2025-12-02 12:00:47 +0530

    vite updated

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,15 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+  // ✅ Add here (INSIDE the config object)
+  css: {
+    devSourcemap: false, // removes source maps in production
+  },
+
+  build: {
+    cssCodeSplit: true, // splits CSS → removes render-blocking 35 KB file
+    minify: "esbuild",
+    sourcemap: false,
+  },
 });
