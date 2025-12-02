4885854f85b111d57279d7d4109f0e85131e081d
Ranjeet Yadav <rj7075yadav@gmail.com>
2025-12-02 15:01:50 +0530

    file updates

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,9 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
 
   // ✅ Add here (INSIDE the config object)
+  css: {
+    devSourcemap: false, // removes source maps in production
+  },
 
   build: {
     cssCodeSplit: true, // splits CSS → removes render-blocking 35 KB file
