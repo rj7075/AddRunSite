a3f57019aaeb973377b294bd2c7dcf876d4a8da4
Ranjeet Yadav <rj7075yadav@gmail.com>
2025-12-02 11:57:05 +0530

    files updated

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import "@fortawesome/fontawesome-free/css/all.min.css";
-import "./fontawesome-fix.css";
+// import "@fortawesome/fontawesome-free/css/all.min.css";
+// import "./fontawesome-fix.css";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
